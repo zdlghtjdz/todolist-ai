f05f0909b18b1f97c6ecaef1df892e4894a049ac
Your Name <you@example.com>
2025-02-18 01:09:10 +0900

    init: 프로젝트 초기 설정

---
+++ src/test/java/com/github/soh/todolist/todolist_ai/TodolistAiApplicationTests.java
@@ -0,0 +1,17 @@
+package com.github.soh.todolist.todolist_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("tests")
+class TodolistAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+
+
+	}
+
+}
