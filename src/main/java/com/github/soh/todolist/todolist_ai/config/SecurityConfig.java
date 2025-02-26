e8dab77b80d3956149127a83044e9ed049f9efda
leehoseong <zdlghtjdz@naver.com>
2025-02-27 01:00:46 +0900

    1. SecurityConfig 수정 처리. 현재 swagger에서 서버응답 시 정상 응답 가능(401 오류 해결)

--- src/main/java/com/github/soh/todolist/todolist_ai/config/SecurityConfig.java
+++ src/main/java/com/github/soh/todolist/todolist_ai/config/SecurityConfig.java
@@ -1,4 +1,18 @@
 package com.github.soh.todolist.todolist_ai.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+
+    @Bean @Deprecated
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests().requestMatchers("/swagger-ui/**","/v3/api-docs/**", "/tasks/**").permitAll().anyRequest().authenticated().and().httpBasic();
+
+        return http.build();
+    }
+
 }
