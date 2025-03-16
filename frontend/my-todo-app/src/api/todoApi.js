838fcf4c11958313d11e30b25c8c54e2417eb8e0
leehoseong <zdlghtjdz@naver.com>
2025-03-16 23:37:09 +0900

    1. todoApi.js 수정. 현재 backend 서버로 데이터 조회되는 것 확인.

--- frontend/my-todo-app/src/api/todoApi.js
+++ frontend/my-todo-app/src/api/todoApi.js
@@ -1,9 +1,11 @@
 
-const API_BASE_URL = "http://localhost:8080"
+//const API_BASE_URL = "http://localhost:8080";
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
 
 export const fetchTodos = async () => {
-
-    const response = await fetch('${API_BASE_URL}/tasks');
+    console.log("API_BASE_URL", API_BASE_URL);
+    const response = await fetch(`${API_BASE_URL}/tasks`);
     if(!response.ok) {
         throw new Error("Failed to fetch tasks");
     }
