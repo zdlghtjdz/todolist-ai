f201891de3e8a0a04a62390b5fb4ad71553ce7d1
leehoseong <zdlghtjdz@naver.com>
2025-03-18 23:03:26 +0900

    vs코드 환경 설정 및 추가버튼 기능 구현 완료료

--- frontend/my-todo-app/src/App.jsx
+++ frontend/my-todo-app/src/App.jsx
@@ -1,30 +1,34 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 import { fetchTodos } from "./api/todoAPi";
 import TodoList from "./components/TodoList";
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
   const [tasks, setTasks] = useState([]);
 
-
   useEffect(() => {
-            fetchTodos().then(setTasks).catch((error) => console.error("Error fetching tasks.", error) );
-       }, []);
+    fetchTodos()
+      .then(setTasks)
+      .catch((error) => console.error("Error fetching tasks.", error));
+  }, []);
 
-      return (
-
-              <div>
-                  <h1>To-Do List</h1>
-                  <ul>
-                      {tasks.map((task) => (
-                          <li key={task.id}>{task.title} - {task.status}</li>
-                      ))}
-                  </ul>
-              </div>
-
-            );
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <ul>
+        {tasks.map((task) => (
+          <>
+            <li key={task.id}>
+              {task.title} - {task.status}
+            </li>
+          </>
+        ))}
+      </ul>
+      <TodoList />
+    </div>
+  );
 }
 
 export default App;
