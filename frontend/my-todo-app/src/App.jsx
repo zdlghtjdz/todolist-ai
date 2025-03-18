cc277f5d8f61a8fb2489a2eee1529d4cddf108d9
leehoseong <zdlghtjdz@naver.com>
2025-03-19 00:06:37 +0900

    삭제 기능 추가 중

--- frontend/my-todo-app/src/App.jsx
+++ frontend/my-todo-app/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { fetchTodos } from "./api/todoAPi";
+import { fetchTodos, deleteTodo } from "./api/todoAPi";
 import TodoList from "./components/TodoList";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
@@ -14,6 +14,14 @@ function App() {
       .catch((error) => console.error("Error fetching tasks.", error));
   }, []);
 
+  const handleDelete = async function () {
+    try {
+      await deleteTodo(task.id);
+    } catch (error) {
+      console.error("Error fetching tasks.", error);
+    }
+  };
+
   return (
     <div>
       <h1>To-Do List</h1>
@@ -22,6 +30,7 @@ function App() {
           <>
             <li key={task.id}>
               {task.title} - {task.status}
+              <button onClick={handleDelete()}>삭제</button>
             </li>
           </>
         ))}
